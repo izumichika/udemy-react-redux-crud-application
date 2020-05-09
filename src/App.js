9c06d91c66ff8874a77e8d757d787a99bfaac255
izumichika <chikara0707@outlook.jp>
2020-05-09 16:27:22 +0900

    laern jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return <h1>Hello World!</h1>;
+
+class App extends Component {
+  render() {
+    return(
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={ () => {console.log("こんにちは")}}/>
+      </React.Fragment>
+    ) 
+  }
 }
 
 export default App;
